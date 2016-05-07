dc123e6a16b162f3ed23cf2e0ab7230a642462a9
Luka Kolar <kolarluka96@gmail.com>
2016-05-07 12:08:52 +0000

    Dodano dodajanje strank v bazo in prikaz ustreznega sporocila

--- eracun.js
+++ eracun.js
@@ -195,6 +195,10 @@ var vrniRacune = function(callback) {
   );
 }
 
+// Sporocilo
+var globalSporociloAfter = ""
+var globalSporocilo = "";
+
 // Registracija novega uporabnika
 streznik.post('/prijava', function(zahteva, odgovor) {
   var form = new formidable.IncomingForm();
@@ -208,22 +212,34 @@ streznik.post('/prijava', function(zahteva, odgovor) {
     	  Address, City, State, Country, PostalCode, \
     	  Phone, Fax, Email, SupportRepId) \
         VALUES (?,?,?,?,?,?,?,?,?,?,?,?)");
-      //TODO: add fields and finalize
-      //stmt.run("", "", "", "", "", "", "", "", "", "", "", 3); 
-      //stmt.finalize();
+      console.log(polja["FirstName"]);
+      stmt.run(polja["FirstName"], polja["LastName"], polja["Company"], polja["Address"], 
+      polja["City"], polja["State"], polja["Country"], polja["PostalCode"], polja["Phone"], 
+      polja["Fax"], polja["Email"], 3); 
+      stmt.finalize();
     } catch (err) {
       napaka2 = true;
+      console.log(err);
     }
-  
+    
+    if (!napaka2) {
+      globalSporocilo = "Stranka je bila uspešno registrirana.";
+    } else {
+      console.log("Napaka");
+      globalSporocilo = "Prišlo je do napake pri registraciji nove stranke. Prosim preverite vnešene podatke in poskusite znova.";
+    }
+    odgovor.redirect(zahteva.get('referer'));
     odgovor.end();
   });
 })
 
 // Prikaz strani za prijavo
 streznik.get('/prijava', function(zahteva, odgovor) {
+  globalSporociloAfter = globalSporocilo;
+  globalSporocilo = "";
   vrniStranke(function(napaka1, stranke) {
       vrniRacune(function(napaka2, racuni) {
-        odgovor.render('prijava', {sporocilo: "", seznamStrank: stranke, seznamRacunov: racuni});  
+        odgovor.render('prijava', {sporocilo: globalSporociloAfter, seznamStrank: stranke, seznamRacunov: racuni});  
       }) 
     });
 })
